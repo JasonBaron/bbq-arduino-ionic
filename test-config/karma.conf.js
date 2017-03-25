0dbfde58f0dabcfed7c53fbf4768683f30267366
Michael Wagner <mdwranger@gmail.com>
2017-03-25 03:18:13 -0400

    Configured browser tests for Travis CI

--- test-config/karma.conf.js
+++ test-config/karma.conf.js
@@ -34,9 +34,7 @@ module.exports = function (config) {
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false
+    autoWatch: false,
   };
 
   config.set(_config);
